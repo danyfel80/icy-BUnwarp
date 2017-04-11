353c4669c1e0d9ea4808702c99a5a5bace4e1fdd
Daniel Felipe Gonzalez Obando <danyfel80@gmail.com>
2017-04-11 14:29:25 +0200

    Fixed null image input check error

--- BUnwarp/src/plugins/danyfel80/registration/bunwarp/BigImageBUnwarp.java
+++ BUnwarp/src/plugins/danyfel80/registration/bunwarp/BigImageBUnwarp.java
@@ -288,11 +288,11 @@ public class BigImageBUnwarp extends BUnwarp {
 	private int validateInput() {
 		@SuppressWarnings("resource")
 		LociImporterPlugin p = new LociImporterPlugin();
-		if (!p.acceptFile(inSrcFile.getValue().getPath())) {
+		if (inSrcFile.getValue() == null || !p.acceptFile(inSrcFile.getValue().getPath())) {
 			MessageDialog.showDialog("Error", "Invalid source file.", MessageDialog.ERROR_MESSAGE);
 			return 1;
 		}
-		if (!p.acceptFile(inTgtFile.getValue().getPath())) {
+		if (inTgtFile.getValue() == null || !p.acceptFile(inTgtFile.getValue().getPath())) {
 			MessageDialog.showDialog("Error", "Invalid target file.", MessageDialog.ERROR_MESSAGE);
 			return 1;
 		}
